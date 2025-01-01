19fc5b63e9fc72c34fbb8bdf60abd53886132657
arobaseSuulei <souleymanediallo2004@outlook.fr>
2025-01-01 14:47:59 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Chemin vers vos fichiers HTML ou JS
+    "./*.html", // Optionnel pour les fichiers HTML à la racine
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
