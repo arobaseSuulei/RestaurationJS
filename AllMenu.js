7fccee503ddb71b814c1abd970114dce5f8387c4
issame-ariba <issameariba@gmail.com>
2025-01-05 17:54:49 +0100

    ajout menus en cours

--- AllMenu.js
+++ AllMenu.js
@@ -1,6 +1,7 @@
 async function getMenu() {
 
 
+    const test = true
     let nameMenu = document.getElementById("nameMenu")
     let name = nameMenu.value
     console.log(name)
@@ -15,16 +16,43 @@ async function getMenu() {
 
         const menus=await response.json()
 
-        console.log(menus.meals[0])
+        
         console.log(menus.meals[0].strMeal)
+
+        if(menus.meals[0] ){
+            const elt = document.createElement("p")
+            
+            const text=document.createTextNode(menus.meals[0].strMeal + " " + "ajouté avec succès ")
+
+
+            elt.appendChild(text)
+            document.getElementById("divAjout").appendChild(elt)
+
+
+        }
+
+        
+        
+
+        
+
+        
+
     }
     catch{
         console.log("error")
     }
 
+    
+
+
+    
+
+
 
 
 }
+
 
 
 
